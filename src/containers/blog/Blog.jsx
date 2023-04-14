7126328a93fc48610e1c58e976790eb3ffda941b
Akshay Kumar Sharma <as906871@gmail.com>
2023-04-14 18:57:10 +0530

    responsive

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -10,9 +10,9 @@ const Blog = () => (
     </div>
     <div className="gpt3__blog-container">
       <div className="gpt3__blog-container_groupB">
-        <Article imgUrl={blog02} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-        <Article imgUrl={blog03} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
-        <Article imgUrl={blog04} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        <Article imgUrl={blog02} date="Sep 26, 2021" text="Learn Something new for the future. Let us exlore how it is?" />
+        <Article imgUrl={blog03} date="Sep 26, 2021" text="Learn Something new for the future. Let us exlore how it is?" />
+        <Article imgUrl={blog04} date="Sep 26, 2021" text="Learn Something new for the future. Let us exlore how it is?" />
 
       </div>
     </div>
